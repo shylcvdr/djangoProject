63830b76b9ec701ac2eb05a53235d9d04999f17f
shylcvdr <suheylacavdar2002@gmail.com>
2022-12-20 20:12:25 +0300

    tablo treatment

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('treatment/', include('treatment.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
+    path(' ',include('home.urls')),
 ]
