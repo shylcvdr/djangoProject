39ebd54cdd7cfca15dc565e2894bfdc7431dc604
shylcvdr <suheylacavdar2002@gmail.com>
2022-12-21 20:19:56 +0300

    slider

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT
+
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'uploads')
