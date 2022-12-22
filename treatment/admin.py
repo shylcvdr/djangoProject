11765c0b6f5afdb45107739ae92f64b55a987266
shylcvdr <suheylacavdar2002@gmail.com>
2022-12-22 23:27:19 +0300

    admin içi

--- treatment/admin.py
+++ treatment/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-from treatment.models import Category, Treatment
+from treatment.models import Category, Treatment, Images
 
 
 # Register your models here.
+
+
+
+class TreatmentImageInline(admin.TabularInline):
+    model=Images
+    extra=5
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
@@ -11,5 +18,11 @@ class TreatmentAdmin(admin.ModelAdmin):
     list_display = ['title', 'status','category','price','amount']
     list_filter = ['status','category']
 
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'treatment','image','image_tag']
+    Inlines = [TreatmentImageInline]
+
+
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Treatment,TreatmentAdmin)
+admin.site.register(Images,ImagesAdmin)
--- treatment/models.py
+++ treatment/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+from django.urls import *
+
 
 # Create your models here.
 class Category(models.Model):
@@ -9,31 +12,51 @@ class Category(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     descriptions = models.CharField(max_length=255)
-    image=models.ImageField(blank=True,upload_to='images/')
-    status=models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
 
     slug = models.SlugField()
-    parent =models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
 class Treatment(models.Model):
     STATUS = (
         ('True', 'Evet'),
-        ('False','Hayır'),
+        ('False', 'Hayır'),
     )
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)#relation with category table
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with category table
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image=models.ImageField(blank=True,upload_to='images/')
+    image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     amount = models.IntegerField()
-    detail=models.TextField()
-    status=models.CharField(max_length=10,choices=STATUS)
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src"{}"height="50"/>'.format(self.image.url))
+
+
+class Images(models.Model):
+    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src"{}"height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
